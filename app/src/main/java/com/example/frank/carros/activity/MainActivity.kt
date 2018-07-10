aa3dadb4384884288f957c83b13d6c73ee3af911
franknfjr <frankdonascimentojr@gmail.com>
2018-07-10 19:06:05 -0300

    214 - setup navdrawer method

--- app/src/main/java/com/example/frank/carros/activity/MainActivity.kt
+++ app/src/main/java/com/example/frank/carros/activity/MainActivity.kt
@@ -1,15 +1,64 @@
 package com.example.frank.carros.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.example.frank.carros.R
 import com.example.frank.carros.extensions.setupToolbar
+import com.example.frank.carros.extensions.toast
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupToolbar(R.id.toolbar)
+        setupNavDrawer()
     }
+
+    // configura o Navigation Drawer
+    private fun setupNavDrawer() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        var drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    // trata o evento do Navigation Drawer
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item_carros_todos -> {
+                toast("Clicou em Carros")
+            }
+            R.id.nav_item_carros_classicos -> {
+                toast("Clicou em Carros Clássicos")
+            }
+            R.id.nav_item_carros_esportivos -> {
+                toast("Clicou em Carros Esportivos")
+            }
+            R.id.nav_item_carros_luxo -> {
+                toast("Clicou em Carros de Luxo")
+            }
+            R.id.nav_item_site_livro -> {
+                toast("Clicou em Github")
+            }
+            R.id.nav_item_settings -> {
+                toast("Clicou em Configurações")
+            }
+        }
+        // fecha o menu depois de tratar o evento
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+
 }
