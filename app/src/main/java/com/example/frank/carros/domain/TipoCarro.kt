79330beab0f1dc575bcac24ebcf427c569c4cdf1
franknfjr <frankdonascimentojr@gmail.com>
2018-07-10 19:14:47 -0300

    217 - Enum TipoCarro

---
+++ app/src/main/java/com/example/frank/carros/domain/TipoCarro.kt
@@ -0,0 +1,9 @@
+package com.example.frank.carros.domain
+
+import com.example.frank.carros.R
+
+enum class TipoCarro(val string: Int){
+    classico(R.string.classico),
+    esportivo(R.string.esportivo),
+    luxo(R.string.luxo)
+}
